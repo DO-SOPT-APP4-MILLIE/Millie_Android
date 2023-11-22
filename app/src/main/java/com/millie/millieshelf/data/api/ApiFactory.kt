413c97585801ddca8f4df1a9d6896d1b643a5ea1
KwakEuiJin <yuijin0909@gmail.com>
2023-11-22 16:11:05 +0900

    [feat/#1_server_setting]: Add intercepter

--- app/src/main/java/com/millie/millieshelf/data/api/ApiFactory.kt
+++ app/src/main/java/com/millie/millieshelf/data/api/ApiFactory.kt
@@ -1,9 +1,13 @@
 package com.millie.millieshelf.data.api
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.millie.millieshelf.BuildConfig
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
 
 object ApiFactory {
 
@@ -13,9 +17,27 @@ object ApiFactory {
     fun getRetrofit(baseUrl: String): Retrofit {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
+            .client(provideOkHttpClient())
             .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
             .build()
     }
+
+    private fun getLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(
+            if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            },
+        )
+    }
+
+    private fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(getLoggingInterceptor())
+        .connectTimeout(60, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .writeTimeout(15, TimeUnit.SECONDS)
+        .build()
 
     inline fun <reified T> create(): T = retrofit.create(T::class.java)
 }
