c181546c835e52aa71d5d3df2178ae5745839527
Dongmin <pdm001125@naver.com>
2023-11-28 17:22:34 +0900

    feature/feat_today_best_viewpager2_rv_item

---
+++ app/src/main/java/com/millie/millieshelf/model/response/TodayBest.kt
@@ -0,0 +1,16 @@
+package com.millie.millieshelf.model.response
+
+data class TodayBest(
+    val code: Int,
+    val data: List<Data>,
+    val msg: String
+) {
+    data class Data(
+        val id: Long,
+        val title: String,
+        val thumbnail: String,
+        val author: String,
+        val completionRate: Int,
+        val readingTime: Int,
+    )
+}
--- app/src/main/java/com/millie/millieshelf/presentation/todayTop/TodayTopViewModel.kt
+++ app/src/main/java/com/millie/millieshelf/presentation/todayTop/TodayTopViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import com.millie.millieshelf.R
 import com.millie.millieshelf.model.TodayAttentionModel
 import com.millie.millieshelf.model.TodayBookmarkModel
+import com.millie.millieshelf.model.response.TodayBest
 
 class TodayTopViewModel : ViewModel() {
     val mockTodayBookmarkList: List<TodayBookmarkModel> = listOf(
@@ -15,6 +16,82 @@ class TodayTopViewModel : ViewModel() {
         TodayBookmarkModel(R.drawable.ic_today_bookmark_thumbs_up, "밀리 아티클"),
         TodayBookmarkModel(R.drawable.ic_today_bookmark_thumbs_up, "매거진 소식"),
     )
+
+    val mockTodayBestList: List<TodayBest.Data> = listOf(
+        TodayBest.Data(
+            id = 1,
+            title = "트렌드 코리아 2023",
+            thumbnail = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.velog.io%2Fimages%2Fmouse0429%2Fpost%2F19b9158e-731a-4e60-ab43-efcdafd9244e%2F%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C.png&tbnid=bW7h1y8puOQadM&vet=12ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ..i&imgrefurl=https%3A%2F%2Fvelog.io%2F%40mouse0429%2FGithub%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C-%25EA%25B8%25B0%25EB%25B3%25B8-%25EA%25B0%259C%25EB%2585%2590&docid=0x91vF8EDsf2YM&w=2494&h=1403&q=%EA%B9%83%ED%97%88%EB%B8%8C&ved=2ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ",
+            author = "temp",
+            completionRate = 42,
+            readingTime = 134
+        ),
+        TodayBest.Data(
+            id = 2,
+            title = "트렌드 코리아 2023",
+            thumbnail = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.velog.io%2Fimages%2Fmouse0429%2Fpost%2F19b9158e-731a-4e60-ab43-efcdafd9244e%2F%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C.png&tbnid=bW7h1y8puOQadM&vet=12ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ..i&imgrefurl=https%3A%2F%2Fvelog.io%2F%40mouse0429%2FGithub%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C-%25EA%25B8%25B0%25EB%25B3%25B8-%25EA%25B0%259C%25EB%2585%2590&docid=0x91vF8EDsf2YM&w=2494&h=1403&q=%EA%B9%83%ED%97%88%EB%B8%8C&ved=2ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ",
+            author = "temp",
+            completionRate = 42,
+            readingTime = 134
+        ),
+        TodayBest.Data(
+            id = 3,
+            title = "트렌드 코리아 2023",
+            thumbnail = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.velog.io%2Fimages%2Fmouse0429%2Fpost%2F19b9158e-731a-4e60-ab43-efcdafd9244e%2F%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C.png&tbnid=bW7h1y8puOQadM&vet=12ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ..i&imgrefurl=https%3A%2F%2Fvelog.io%2F%40mouse0429%2FGithub%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C-%25EA%25B8%25B0%25EB%25B3%25B8-%25EA%25B0%259C%25EB%2585%2590&docid=0x91vF8EDsf2YM&w=2494&h=1403&q=%EA%B9%83%ED%97%88%EB%B8%8C&ved=2ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ",
+            author = "temp",
+            completionRate = 42,
+            readingTime = 134
+        ),
+        TodayBest.Data(
+            id = 4,
+            title = "트렌드 코리아 2023",
+            thumbnail = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.velog.io%2Fimages%2Fmouse0429%2Fpost%2F19b9158e-731a-4e60-ab43-efcdafd9244e%2F%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C.png&tbnid=bW7h1y8puOQadM&vet=12ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ..i&imgrefurl=https%3A%2F%2Fvelog.io%2F%40mouse0429%2FGithub%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C-%25EA%25B8%25B0%25EB%25B3%25B8-%25EA%25B0%259C%25EB%2585%2590&docid=0x91vF8EDsf2YM&w=2494&h=1403&q=%EA%B9%83%ED%97%88%EB%B8%8C&ved=2ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ",
+            author = "temp",
+            completionRate = 42,
+            readingTime = 134
+        ),
+        TodayBest.Data(
+            id = 5,
+            title = "트렌드 코리아 2023",
+            thumbnail = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.velog.io%2Fimages%2Fmouse0429%2Fpost%2F19b9158e-731a-4e60-ab43-efcdafd9244e%2F%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C.png&tbnid=bW7h1y8puOQadM&vet=12ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ..i&imgrefurl=https%3A%2F%2Fvelog.io%2F%40mouse0429%2FGithub%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C-%25EA%25B8%25B0%25EB%25B3%25B8-%25EA%25B0%259C%25EB%2585%2590&docid=0x91vF8EDsf2YM&w=2494&h=1403&q=%EA%B9%83%ED%97%88%EB%B8%8C&ved=2ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ",
+            author = "temp",
+            completionRate = 42,
+            readingTime = 134
+        ),
+        TodayBest.Data(
+            id = 6,
+            title = "트렌드 코리아 2023",
+            thumbnail = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.velog.io%2Fimages%2Fmouse0429%2Fpost%2F19b9158e-731a-4e60-ab43-efcdafd9244e%2F%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C.png&tbnid=bW7h1y8puOQadM&vet=12ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ..i&imgrefurl=https%3A%2F%2Fvelog.io%2F%40mouse0429%2FGithub%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C-%25EA%25B8%25B0%25EB%25B3%25B8-%25EA%25B0%259C%25EB%2585%2590&docid=0x91vF8EDsf2YM&w=2494&h=1403&q=%EA%B9%83%ED%97%88%EB%B8%8C&ved=2ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ",
+            author = "temp",
+            completionRate = 42,
+            readingTime = 134
+        ),
+        TodayBest.Data(
+            id = 7,
+            title = "트렌드 코리아 2023",
+            thumbnail = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.velog.io%2Fimages%2Fmouse0429%2Fpost%2F19b9158e-731a-4e60-ab43-efcdafd9244e%2F%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C.png&tbnid=bW7h1y8puOQadM&vet=12ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ..i&imgrefurl=https%3A%2F%2Fvelog.io%2F%40mouse0429%2FGithub%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C-%25EA%25B8%25B0%25EB%25B3%25B8-%25EA%25B0%259C%25EB%2585%2590&docid=0x91vF8EDsf2YM&w=2494&h=1403&q=%EA%B9%83%ED%97%88%EB%B8%8C&ved=2ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ",
+            author = "temp",
+            completionRate = 42,
+            readingTime = 134
+        ),
+        TodayBest.Data(
+            id = 8,
+            title = "트렌드 코리아 2023",
+            thumbnail = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.velog.io%2Fimages%2Fmouse0429%2Fpost%2F19b9158e-731a-4e60-ab43-efcdafd9244e%2F%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C.png&tbnid=bW7h1y8puOQadM&vet=12ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ..i&imgrefurl=https%3A%2F%2Fvelog.io%2F%40mouse0429%2FGithub%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C-%25EA%25B8%25B0%25EB%25B3%25B8-%25EA%25B0%259C%25EB%2585%2590&docid=0x91vF8EDsf2YM&w=2494&h=1403&q=%EA%B9%83%ED%97%88%EB%B8%8C&ved=2ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ",
+            author = "temp",
+            completionRate = 42,
+            readingTime = 134
+        ),
+        TodayBest.Data(
+            id = 9,
+            title = "트렌드 코리아 2023",
+            thumbnail = "https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.velog.io%2Fimages%2Fmouse0429%2Fpost%2F19b9158e-731a-4e60-ab43-efcdafd9244e%2F%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C.png&tbnid=bW7h1y8puOQadM&vet=12ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ..i&imgrefurl=https%3A%2F%2Fvelog.io%2F%40mouse0429%2FGithub%25EA%25B9%2583%25ED%2597%2588%25EB%25B8%258C-%25EA%25B8%25B0%25EB%25B3%25B8-%25EA%25B0%259C%25EB%2585%2590&docid=0x91vF8EDsf2YM&w=2494&h=1403&q=%EA%B9%83%ED%97%88%EB%B8%8C&ved=2ahUKEwil4u6emuaCAxX9Z_UHHZpID9YQMygBegQIARBZ",
+            author = "temp",
+            completionRate = 42,
+            readingTime = 134
+        ),
+    )
+
 
     val mockTodayAttentionList: List<TodayAttentionModel> = listOf(
         TodayAttentionModel(0, R.drawable.iv_today_attention_img_1),
