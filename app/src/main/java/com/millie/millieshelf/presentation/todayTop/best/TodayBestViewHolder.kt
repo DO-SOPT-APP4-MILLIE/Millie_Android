e8ff5818f799a45e4b467a57eac15da72a43cb0c
Dongmin <pdm001125@naver.com>
2023-11-29 11:09:54 +0900

    feature/feat_today_hard_rank

--- app/src/main/java/com/millie/millieshelf/presentation/todayTop/best/TodayBestViewHolder.kt
+++ app/src/main/java/com/millie/millieshelf/presentation/todayTop/best/TodayBestViewHolder.kt
@@ -1,15 +1,25 @@
 package com.millie.millieshelf.presentation.todayTop.best
 
+import android.graphics.Color
 import androidx.recyclerview.widget.RecyclerView
 import com.millie.millieshelf.R
 import com.millie.millieshelf.databinding.ItemTodayBestContentBinding
 import com.millie.millieshelf.model.response.TodayBest
+import kotlin.random.Random
 
 class TodayBestViewHolder(private val binding: ItemTodayBestContentBinding) :
     RecyclerView.ViewHolder(binding.root) {
     fun onBind(data: TodayBest.Data) =
         with(binding) {
             tvTodayBestRank.text = data.id.toString()
+            val rand = Random.nextInt(3)
+            tvTodayBestRankChange.text = when(rand){
+                0 -> "-"
+                1 -> "New"
+                2 -> "▾ 1"
+                else -> "▴ 1"
+            }
+            //tvTodayBestRankChange.setTextColor(rand값에 맞는 색상)
             //ivTodayBestBook.load(data.thumbnail)
             tvTodayBestBookName.text = data.title
             tvTodayBestBookAuthor.text = data.author
