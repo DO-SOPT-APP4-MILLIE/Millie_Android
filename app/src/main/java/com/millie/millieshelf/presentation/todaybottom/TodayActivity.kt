151c4c42bdef3b7aebec2a91222ef8dfd50b5834
Cat-JiYoung <kimyamae82@gmail.com>
2023-11-29 18:05:37 +0900

    [feat] 바텀네비 플로팅버튼 애니메이션 추가

--- app/src/main/java/com/millie/millieshelf/presentation/todaybottom/TodayActivity.kt
+++ app/src/main/java/com/millie/millieshelf/presentation/todaybottom/TodayActivity.kt
@@ -1,6 +1,8 @@
 package com.millie.millieshelf.presentation.todaybottom
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
@@ -11,11 +13,13 @@ import com.millie.millieshelf.databinding.ActivityTodayBinding
 class TodayActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityTodayBinding
+    private var isExpanded = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTodayBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        clickFabBtn()
         initBottomNavigation()
     }
 
@@ -34,6 +38,33 @@ class TodayActivity : AppCompatActivity() {
     private inline fun <reified T : Fragment> navigateTo() {
         supportFragmentManager.commit {
             replace<T>(R.id.fcv_today, T::class.simpleName)
+        }
+    }
+
+    private fun clickFabBtn() {
+        binding.fabMain.setOnClickListener {
+            if (isExpanded) {
+                binding.clFabAudiobook.visibility = View.VISIBLE
+                binding.clFabStory.visibility = View.VISIBLE
+                binding.clFabMillieroad.visibility = View.VISIBLE
+
+                ObjectAnimator.ofFloat(binding.clFabAudiobook, "translationY", 0f).apply { start() }
+                ObjectAnimator.ofFloat(binding.clFabMillieroad, "translationY", 0f)
+                    .apply { start() }
+                ObjectAnimator.ofFloat(binding.clFabStory, "translationY", 0f).apply { start() }
+            } else {
+                binding.clFabAudiobook.visibility = View.INVISIBLE
+                binding.clFabStory.visibility = View.INVISIBLE
+                binding.clFabMillieroad.visibility = View.INVISIBLE
+
+                ObjectAnimator.ofFloat(binding.clFabAudiobook, "translationY", 360f)
+                    .apply { start() }
+                ObjectAnimator.ofFloat(binding.clFabMillieroad, "translationY", 180f)
+                    .apply { start() }
+                ObjectAnimator.ofFloat(binding.clFabStory, "translationY", 90f).apply { start() }
+            }
+
+            isExpanded = !isExpanded
         }
     }
 }
